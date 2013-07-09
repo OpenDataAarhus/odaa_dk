e4a48dd546bdaba4b7b5bac2bdb2e301cb6fdcac
Brian Jensen <mail@magnify.dk>
2013-07-09 11:21:18 +0200

    Added logo, favicon, updated variables with grays, updated button colors

---
+++ themes/odaa/templates/mockups/page.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" version="XHTML+RDFa 1.0" dir="ltr"
+  xmlns:content="http://purl.org/rss/1.0/modules/content/"
+  xmlns:dc="http://purl.org/dc/terms/"
+  xmlns:foaf="http://xmlns.com/foaf/0.1/"
+  xmlns:og="http://ogp.me/ns#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+  xmlns:sioc="http://rdfs.org/sioc/ns#"
+  xmlns:sioct="http://rdfs.org/sioc/types#"
+  xmlns:skos="http://www.w3.org/2004/02/skos/core#"
+  xmlns:xsd="http://www.w3.org/2001/XMLSchema#">
+<head profile="http://www.w3.org/1999/xhtml/vocab">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link rel="shortcut icon" href="/sites/odaa.dk/themes/odaa/favicon.ico" type="image/vnd.microsoft.icon" />
+  <meta name="Generator" content="Drupal 7 (http://drupal.org)" />
+  <meta name="viewport" content="width=device-width">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Welcome to dev.odaa.dk | dev.odaa.dk</title>
+  <link type="text/css" rel="stylesheet" href="/sites/odaa.dk/themes/odaa/css/odaa.styles.css" media="all" />
+</head>
+<body class="html front not-logged-in no-sidebars page-node">
+  <div id="page" class="page">
+    <header class="site-header">
+      <section class="header-wrapper">
+        <div class="header-inner">
+          <a href="/" class="logo"><img alt="" src="/sites/odaa.dk/themes/odaa/logo.png"></a>
+        </div>
+      </section>
+    </header>
+    <div class="content-wrapper">
+      <div class="content-inner">
+        <div class="primary-content">
+          Primary content
+        </div>
+        <div class="secondary-content">
+          Secondary content
+        </div>
+        <div class="tertiary-content">
+          Tertiary content
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
