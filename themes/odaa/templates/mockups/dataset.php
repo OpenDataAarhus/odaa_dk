caa096a4bd7e0c9b7c9472461071cb95c1c45728
Rasmus Frey <rasmus@artz.dk>
2013-07-11 15:22:56 +0200

    add footer to mockups

--- themes/odaa/templates/mockups/dataset.php
+++ themes/odaa/templates/mockups/dataset.php
@@ -167,6 +167,13 @@
         </div>
       </div>
     </div>
+    <footer class="site-footer">
+      <section class="footer-wrapper">
+        <div class="footer-inner">
+          Footer
+        </div>
+      </section>
+    </footer>
   </div>
 </body>
 </html>
