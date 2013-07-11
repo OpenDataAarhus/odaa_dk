be695d910740fe3eaaa02d743ec35ef019be69a4
Brian Jensen <mail@magnify.dk>
2013-07-11 10:15:23 +0200

    Added dataset stylesheet, created border style variable, updated page markup and styles

--- themes/odaa/templates/mockups/dataset.php
+++ themes/odaa/templates/mockups/dataset.php
@@ -19,25 +19,27 @@
     <div class="content-wrapper">
       <div class="content-inner">
         <div class="primary-content">
-          <section class="page-content">
-            <h1 class="page-title"> Bevaringsværdige bygninger i Aarhus Kommune</h1>
-            <p>Datasættet beskriver de bygninger, der er registreret som bevaringsværdige i kommuneatlas (http://gis.aarhus.dk/kommuneatlas/) Mere om vurdering af bevaringsværdi efter SAVE-metoden: https://www.kulturarv.dk/fbb/bevaringsvaerdier.htm</p>
-            <p>Der er bl.a. angivet adresse med vejnavn/vejkode og husnummer. Ved hjælp af datasættet "Koordinatsætning af Aarhus adresser" kan man fange koordinater, så bygningerne kan placeres i et kort.</p>
-          </section>
-          <section class="dataset--data-resources">
-            <h2 class="section-title">Data and Resources</h2>
-            <ul class="dataset--list">
-              <li class="dataset--list-item">
-                <a class="dataset--list-item-heading" title="savebygnings_view.xml" href="/dataset/bevaringsvaerdige-bygninger-i-aarhus-kommune/resource/127751d2-8a48-4c57-a206-e03b4330f317">
-                  savebygnings_view.xml
-                  <span data-format="xml" property="dc:format" class="ckan--format-label">XML</span>
-                </a>
-                <p class="dataset--description"><span class="empty">No description for this resource</span></p>
-                <a class="button" href="/dataset/bevaringsvaerdige-bygninger-i-aarhus-kommune/resource/127751d2-8a48-4c57-a206-e03b4330f317"><i class="icon-bar-chart"></i>Preview/eksempel</a>
-                <a class="button" href="http://www.odaa.dk/storage/f/2013-02-05T10%3A43%3A38.516Z/savebygnings-view.xml"><i class="icon-download"></i>Download</a>
-              </li>
-            </ul>
-          </section>
+          <div class="page--content-wrapper">
+            <section class="page--content">
+              <h1 class="page--title"> Bevaringsværdige bygninger i Aarhus Kommune</h1>
+              <p>Datasættet beskriver de bygninger, der er registreret som bevaringsværdige i kommuneatlas (http://gis.aarhus.dk/kommuneatlas/) Mere om vurdering af bevaringsværdi efter SAVE-metoden: https://www.kulturarv.dk/fbb/bevaringsvaerdier.htm</p>
+              <p>Der er bl.a. angivet adresse med vejnavn/vejkode og husnummer. Ved hjælp af datasættet "Koordinatsætning af Aarhus adresser" kan man fange koordinater, så bygningerne kan placeres i et kort.</p>
+            </section>
+            <section class="dataset--data-resources">
+              <h2 class="section-title">Data and Resources</h2>
+              <ul class="dataset--list">
+                <li class="dataset--list-item">
+                  <a class="dataset--list-item-heading" title="savebygnings_view.xml" href="/dataset/bevaringsvaerdige-bygninger-i-aarhus-kommune/resource/127751d2-8a48-4c57-a206-e03b4330f317">
+                    savebygnings_view.xml
+                    <span data-format="xml" property="dc:format" class="ckan--format-label">XML</span>
+                  </a>
+                  <p class="dataset--description"><span class="empty">No description for this resource</span></p>
+                  <a class="button" href="/dataset/bevaringsvaerdige-bygninger-i-aarhus-kommune/resource/127751d2-8a48-4c57-a206-e03b4330f317"><i class="icon-bar-chart"></i>Preview/eksempel</a>
+                  <a class="button" href="http://www.odaa.dk/storage/f/2013-02-05T10%3A43%3A38.516Z/savebygnings-view.xml"><i class="icon-download"></i>Download</a>
+                </li>
+              </ul>
+            </section>
+          </div>
         </div>
         <div class="secondary-content">
           <?php include 'snippets/sub-menu.inc'; ?>
