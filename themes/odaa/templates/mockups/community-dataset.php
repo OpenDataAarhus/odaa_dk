bfab3f96c25aa66f1ac58b96f97139afbfee3e17
Brian Jensen <mail@magnify.dk>
2013-07-12 14:41:46 +0200

    Moved community styling to seperate file, reworked filter adn filter menu styling

--- themes/odaa/templates/mockups/community-dataset.php
+++ themes/odaa/templates/mockups/community-dataset.php
@@ -33,12 +33,12 @@
                 </form>
               </div>
               <div class="community--actions">
-                <a href="#" class="community--action-sort-latest active"><i class="icon-ok"></i>Sortér: Nyeste</a>
-                <a href="#" class="community--action-sort-hot"><i class="icon-comments-alt"></i>Sortér: Mest kommenterede</a>
+                <a href="#" class="community--action-sort-latest"><i class="icon-time"></i>Sortér: Nyeste</a>
+                <a href="#" class="community--action-sort-hot active"><i class="icon-ok"></i>Sortér: Mest kommenterede</a>
                 <a href="#" class="community--action-create"><i class="icon-plus"></i>Opret indlæg</a>
               </div>
               <section class="community--list-wrapper">
-                <span class="community--list-heading">Filter:</span><span class="community--empty-text">Ingen</span>
+                <span class="community--list-heading">Filter:</span><a href="#" class="community--tag"><i class="icon-remove"></i>Datasæt</a>
                 <ul class="community--list">
                   <li class="community--list-item">
                     <div class="community--list-icon"><a href="#"><i class="icon-file"></i></a></div>
@@ -53,16 +53,15 @@
                     </div>
                   </li>
                   <li class="community--list-item">
-                    <div class="community--list-icon"><a href="#"><i class="icon-comment"></i></a></div>
+                    <div class="community--list-icon"><a href="#"><i class="icon-file"></i></a></div>
                     <div class="community--list-content">
-                      <h3 class="community--list-header"><a href="#">Gode idéer?</a></h3>
-                      <p>Jeg har tid og er klar på at lave noget sjovt med noget åben data. Nogen der har nogle gode idéer?</p>
+                      <h3 class="community--list-header"><a href="#">Bålpladser i Aarhus</a></h3>
                       <div class="community--list-meta">
                         <span class="community--list-posted">20. juli 2013 @ 14:57</span>|<a href="#" class="community--list-author">User name</a>
                       </div>
                     </div>
                     <div class="community--list-footer">
-                      <a href="#" class="community--list-comments-link">0 kommentarer</a>|<a href="#" class="community--list-read-more">Læs mere</a>
+                      <a href="#" class="community--list-comments-link">12 kommentarer</a>|<a href="#" class="community--list-read-more">Læs mere</a>
                     </div>
                   </li>
                   <li class="community--list-item">
@@ -78,33 +77,7 @@
                     </div>
                   </li>
                   <li class="community--list-item">
-                    <div class="community--list-icon"><a href="#"><i class="icon-user"></i></a></div>
-                    <div class="community--list-content">
-                      <h3 class="community--list-header"><a href="#">Proof of cencept på data fra skraldespande</a></h3>
-                      <p>Donec ullamcorper nulla non metus auctor fringilla. Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Curabitur blandit tempus porttitor. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
-                      <div class="community--list-meta">
-                        <span class="community--list-posted">20. juli 2013 @ 14:57</span>|<a href="#" class="community--list-author">User name</a>
-                      </div>
-                    </div>
-                    <div class="community--list-footer">
-                      <a href="#" class="community--list-comments-link">12 kommentarer</a>|<a href="#" class="community--list-read-more">Læs mere</a>
-                    </div>
-                  </li>
-                  <li class="community--list-item">
-                    <div class="community--list-icon"><a href="#"><i class="icon-comment"></i></a></div>
-                    <div class="community--list-content">
-                      <h3 class="community--list-header"><a href="#">Gode idéer?</a></h3>
-                      <p>Jeg har tid og er klar på at lave noget sjovt med noget åben data. Nogen der har nogle gode idéer?</p>
-                      <div class="community--list-meta">
-                        <span class="community--list-posted">20. juli 2013 @ 14:57</span>|<a href="#" class="community--list-author">User name</a>
-                      </div>
-                    </div>
-                    <div class="community--list-footer">
-                      <a href="#" class="community--list-comments-link">0 kommentarer</a>|<a href="#" class="community--list-read-more">Læs mere</a>
-                    </div>
-                  </li>
-                  <li class="community--list-item">
-                    <div class="community--list-icon"><a href="#"><i class="icon-file"></a></i></div>
+                    <div class="community--list-icon"><a href="#"><i class="icon-file"></i></a></div>
                     <div class="community--list-content">
                       <h3 class="community--list-header"><a href="#">Bålpladser i Aarhus</a></h3>
                       <div class="community--list-meta">
@@ -125,10 +98,10 @@
             <div class="community--filter-inner">
               <h3 class="community--filter-header"><i class="icon-filter"></i>Filtrér</h3>
               <ul class="community--filter-list">
-                <li class="community--filter-list-item first"><a href="community.php" class="active"><i class="icon-check"></i>Alle</a></li>
+                <li class="community--filter-list-item first"><a href="community.php"><i class="icon-check-empty"></i>Alle</a></li>
                 <li class="community--filter-list-item first"><a href="community-forum.php"><i class="icon-check-empty"></i>Forum</a></li>
                 <li class="community--filter-list-item"><a href="community-use-cases.php"><i class="icon-check-empty"></i>Use cases</a></li>
-                <li class="community--filter-list-item last"><a href="community-dataset.php"><i class="icon-check-empty"></i>Datasæt</a></li>
+                <li class="community--filter-list-item last"><a href="community-dataset.php" class="active"><i class="icon-check"></i>Datasæt</a></li>
               </ul>
             </div>
           </div>
