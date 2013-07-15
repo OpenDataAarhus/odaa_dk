54ddd406d457115ece50eac7195f46ba6df20486
Thomas Johansen <thsj@aarhus.dk>
2013-07-15 15:15:31 +0200

    Removed div from pure template.

--- themes/odaa/templates/mockups/comment.js
+++ themes/odaa/templates/mockups/comment.js
@@ -9,10 +9,10 @@
     var directive = { 
       'ul.comments--list': {
         'i<-data': {
-          'div.comments--list--cid' : 'i.cid',
-          'div.comments--list-author' : 'i.author',
-          'div.comments--list-posted' : 'i.date',
-          'div.comments--list-content' : 'i.subject'
+          '.comments--list--cid' : 'i.cid',
+          '.comments--list-author' : 'i.author',
+          '.comments--list-posted' : 'i.date',
+          '.comments--list-content' : 'i.subject'
         }
       }
     };
