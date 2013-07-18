9a62891bbdadacb52c0fa3111b7f10b8127ef5dc
Brian Jensen <mail@magnify.dk>
2013-07-18 11:21:07 +0200

    Added system messages stylesheet to override

--- themes/odaa/templates/block/block--odaa-ckan-blocks--datasets.tpl.php
+++ themes/odaa/templates/block/block--odaa-ckan-blocks--datasets.tpl.php
@@ -1,8 +1,8 @@
 <div class="<?php print $classes; ?>"<?php print $attributes; ?> rel="<?php print $block_html_id; ?>">
-  <div class="spotbox--inner some-other-class">
+  <div class="search-spotbox--inner">
     <?php print render($title_prefix); ?>
     <?php if ($block->subject): ?>
-      <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
+      <h2 class="search-spotbox--header"><?php print $block->subject ?></h2>
     <?php endif;?>
     <?php print render($title_suffix); ?>
 
