c09efefa8bb6ad32a91c04100b84cdb8f403f4cd
Brian Jensen <mail@magnify.dk>
2013-07-22 10:28:00 +0200

    Moved messages outside primary content

--- themes/odaa/templates/page/page.tpl.php
+++ themes/odaa/templates/page/page.tpl.php
@@ -89,8 +89,8 @@
   </header>
   <div class="content-wrapper">
     <div class="content-inner">
+      <?php print $messages; ?>
       <div class="primary-content">
-        <?php print $messages; ?>
 
         <?php if (!$is_front): ?>
           <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
