f5223ed179191efd45911eb9a22afe92ac565bb5
Brian Jensen <mail@magnify.dk>
2013-07-22 10:30:50 +0200

    Updated mockup: moved spotboxes to tertiary content

--- themes/odaa/templates/mockups/forsiden.php
+++ themes/odaa/templates/mockups/forsiden.php
@@ -34,6 +34,30 @@
               </div>
             </div>
           </div>
+        </div>
+        <div class="secondary-content">
+          <div class="spotbox">
+            <div class="spotbox--inner">
+              <h3 class="spotbox--header">Velkommen til ODAA</h3>
+              <div class="spotbox--image"><a href="#"><img src="http://www.odaa.dk/media/your_rainbow_1_small.jpg" /></a></div>
+              <div class="spotbox--content">
+                <p>ODAA er en open data platform, hvor åbne data bliver gjort frit tilgængelige, så de er nemme for dig at finde.</p>
+                <p>Vi håber, at du vil forvandle de åbne data til nye og innovative services og fortælle om det her på platformen.</p>
+                <p>Du kan læse mere <a href="#">Om ODAA her</a>.</p>
+              </div>
+            </div>
+          </div>
+          <div class="spotbox">
+            <div class="spotbox--inner">
+              <h3 class="spotbox--header"><i class="icon-star-empty"></i>Featured datasæt</h3>
+              <div class="spotbox--content">
+                <h4><a href="#">3D Bygninger - Bymodel</a></h4>
+                <div>3D bygninger for den indre del af Aarhus By (ca. indenfor Ringgaden). Data er på DWG-formatet og er en bygningstrådmodel. Data fylder ca. 50 MB. Vi anbefaler, at du downloader datasættet i Chrome eller Firefox.</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="tertiary-content">
           <div class="spotbox">
             <div class="spotbox--inner">
               <h2 class="spotbox--header"><a href="#"><i class="icon-comments-alt"></i>Seneste kommentarer</a></h2>
@@ -69,62 +93,6 @@
           <div class="spotbox">
             <div class="spotbox--inner">
               <h2 class="spotbox--header"><a href="#"><i class="icon-sitemap"></i>Seneste datasæt</a></h2>
-              <ul class="spotbox--list">
-                <li class="spotbox--list-item first">
-                  <div class="spotbox--list-image"><a href="#comment1"><img src="//gravatar.com/avatar/f9879d71855b5ff21e4963273a886bfc?s=30&amp;d=identicon" /></a></div>
-                  <span class="spotbox--list-link"><a href="#comment1">Morbi leo risus</a></span>
-                  <p class="spotbox--list-posted">54 minutter 32 sekunder siden</p>
-                </li>
-                <li class="spotbox--list-item">
-                  <div class="spotbox--list-image"><a href="#comment2"><img src="//gravatar.com/avatar/3c0c3f0f2bbd8101fcc64928e7768d67?s=30&amp;d=identicon" /></a></div>
-                  <span class="spotbox--list-link"><a href="#comment2">Porta ac consectetur ac</a></span>
-                  <p class="spotbox--list-posted">54 minutter 32 sekunder siden</p>
-                </li>
-                <li class="spotbox--list-item">
-                  <div class="spotbox--list-image"><a href="#comment3"><img src="//gravatar.com/avatar/32fc47cc10bc49b47aa0644d79cf04de?s=30&amp;d=identicon" /></a></div>
-                  <span class="spotbox--list-link"><a href="#comment3">Vestibulum at eros</a></span>
-                  <p class="spotbox--list-posted">54 minutter 32 sekunder siden</p>
-                </li>
-                <li class="spotbox--list-item">
-                  <div class="spotbox--list-image"><a href="#comment4"><img src="//gravatar.com/avatar/3c0c3f0f2bbd8101fcc64928e7768d67?s=30&amp;d=identicon" /></a></div>
-                  <span class="spotbox--list-link"><a href="#comment4">Nibh Dolor Ornare</a></span>
-                  <p class="spotbox--list-posted">54 minutter 32 sekunder siden</p>
-                </li>
-                <li class="spotbox--list-item last">
-                  <div class="spotbox--list-image"><a href="#comment5"><img src="//gravatar.com/avatar/f9879d71855b5ff21e4963273a886bfc?s=30&amp;d=identicon" /></a></div>
-                  <span class="spotbox--list-link"><a href="#comment5">Ridiculus Euismod Lorem</a></span>
-                  <p class="spotbox--list-posted">54 minutter 32 sekunder siden</p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="secondary-content">
-          <div class="spotbox">
-            <div class="spotbox--inner">
-              <h3 class="spotbox--header">Velkommen til ODAA</h3>
-              <div class="spotbox--image"><a href="#"><img src="http://www.odaa.dk/media/your_rainbow_1_small.jpg" /></a></div>
-              <div class="spotbox--content">
-                <p>ODAA er en open data platform, hvor åbne data bliver gjort frit tilgængelige, så de er nemme for dig at finde.</p>
-                <p>Vi håber, at du vil forvandle de åbne data til nye og innovative services og fortælle om det her på platformen.</p>
-                <p>Du kan læse mere <a href="#">Om ODAA her</a>.</p>
-              </div>
-            </div>
-          </div>
-          <div class="spotbox">
-            <div class="spotbox--inner">
-              <h3 class="spotbox--header"><i class="icon-star-empty"></i>Featured datasæt</h3>
-              <div class="spotbox--content">
-                <h4><a href="#">3D Bygninger - Bymodel</a></h4>
-                <div>3D bygninger for den indre del af Aarhus By (ca. indenfor Ringgaden). Data er på DWG-formatet og er en bygningstrådmodel. Data fylder ca. 50 MB. Vi anbefaler, at du downloader datasættet i Chrome eller Firefox.</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="tertiary-content">
-          <div class="spotbox">
-            <div class="spotbox--inner">
-              <h2 class="spotbox--header"><a href="#"><i class="icon-lightbulb"></i>Something else</a></h2>
               <ul class="spotbox--list">
                 <li class="spotbox--list-item first">
                   <div class="spotbox--list-image"><a href="#comment1"><img src="//gravatar.com/avatar/f9879d71855b5ff21e4963273a886bfc?s=30&amp;d=identicon" /></a></div>
