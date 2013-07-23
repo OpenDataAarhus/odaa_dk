88132216263a237a00c538c4e7d235c4e7911a0b
Thomas Johansen <thsj@aarhus.dk>
2013-07-23 14:08:00 +0200

    Cleanup.

--- themes/odaa/scripts/comment.js
+++ themes/odaa/scripts/comment.js
@@ -51,10 +51,6 @@
       $('li.dataset--list-item').each(function (item) {
         // Find hash
         var hash = $(item).attr('data-hash');
-        $.getJSON('/odaa_comment/comment_count/'+hash, function (data) {
-      $('li.dataset--list-item').each(function (id, item) {
-        // Find hash
-        var hash = $(item).attr('data-hash');
         $.getJSON('/odaa_comment/count/'+hash, function (data) {
           if (data.status) {
             var html = $(item).closest('.dataset--list-comments-link').html();
