d952e6d72b80fd935e0dfaaf811f8941dc99ea11
martinydegranath <martinydegranath@172.16.23.173>
2013-07-23 13:18:45 +0200

    Added comment count functionality

--- themes/odaa/scripts/comment.js
+++ themes/odaa/scripts/comment.js
@@ -47,6 +47,16 @@
         return false;
       });
     }
+    else if ($('.dataset--list-wrapper').length) {
+      $('li.dataset--list-item').each(function (item) {
+        // Find hash
+        var hash = $(item).attr('data-hash');
+        $.getJSON('/odaa_comment/comment_count/'+hash, function (data) {
+          $(item).closest('.dataset--list-comments-link').html(data.count);
+        });
+        
+      });
+    }
   });
 
   function delete_comment(cid) {
