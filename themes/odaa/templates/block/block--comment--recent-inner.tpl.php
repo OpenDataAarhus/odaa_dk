720245ce791a050095b1c954fa5dc24b7570d2f6
Thomas Johansen <thsj@aarhus.dk>
2013-07-24 14:38:27 +0200

    Cleanup.

--- themes/odaa/templates/block/block--comment--recent-inner.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @file
- * 
- */
-?>
-<span class="spotbox--list-link"><?php print l($comment->subject, 'comment/' . $comment->cid, array('fragment' => 'comment-' . $comment->cid)); ?></span>
-<p class="spotbox--list-posted"><?php print t('@time ago', array('@time' => format_interval(REQUEST_TIME - $comment->changed))); ?></p>
