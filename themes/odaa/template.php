7848cfa991570a574dc696b685469fe2172c3bbf
Martin Yde <martinydegranath@Martins-MacBook-Pro.local>
2013-07-25 15:07:03 +0200

    changed form html

--- themes/odaa/template.php
+++ themes/odaa/template.php
@@ -197,76 +197,8 @@ function odaa_form_alter(&$form, &$form_state, $form_id){
       $form['actions']['#suffix']  = '</div>';
       break;
       case 'views_exposed_form':
-        echo '<pre>';
-        //print_r($form);
-        echo '</pre>';
+        $form['sort_by']['#attributes']['class'][] = 'search--actions-sort';
+       // $form['#info']['#attributes']['class'][] = 'search-form--content-block--input';
       break;
   }
-}
-
-
-/**
- * Implements hook_form_element().
- */
-
-function odaa_form_element($variables){
- $element = &$variables['element'];
-
-  // This function is invoked as theme wrapper, but the rendered form element
-  // may not necessarily have been processed by form_builder().
-  $element += array(
-    '#title_display' => 'before',
-  );
-
-  // Add element #id for #type 'item'.
-  if (isset($element['#markup']) && !empty($element['#id'])) {
-    $attributes['id'] = $element['#id'];
-  }
-  // Add element's #type and #name as class to aid with JS/CSS selectors.
-  $attributes['class'] = array('form-item');
-  if (!empty($element['#type'])) {
-    $attributes['class'][] = 'form-type-' . strtr($element['#type'], '_', '-');
-  }
-  if (!empty($element['#name'])) {
-    $attributes['class'][] = 'form-item-' . strtr($element['#name'], array(' ' => '-', '_' => '-', '[' => '-', ']' => ''));
-  }
-  // Add a class for disabled elements to facilitate cross-browser styling.
-  if (!empty($element['#attributes']['disabled'])) {
-    $attributes['class'][] = 'form-disabled';
-  }
-  $output ="\n";
-
-  // If #title is not set, we don't display any label or required marker.
-  if (!isset($element['#title'])) {
-    $element['#title_display'] = 'none';
-  }
-  //$prefix = isset($element['#field_prefix']) ? '<span class="field-prefix">' . $element['#field_prefix'] . '</span> ' : '';
-  //$suffix = isset($element['#field_suffix']) ? ' <span class="field-suffix">' . $element['#field_suffix'] . '</span>' : '';
-
-  switch ($element['#title_display']) {
-    case 'before':
-    case 'invisible':
-      $output .= ' ' . theme('form_element_label', $variables);
-      $output .= ' ' . $element['#children'] .  "\n";
-      break;
-
-    case 'after':
-      $output .= ' ' . $element['#children'];
-      $output .= ' ' . theme('form_element_label', $variables) . "\n";
-      break;
-
-    case 'none':
-    case 'attribute':
-      // Output no label and no required marker, only the children.
-      $output .= ' ' . $element['#children'] . "\n";
-      break;
-  }
-
-  if (!empty($element['#description'])) {
-    $output .= '<div class="description">' . $element['#description'] . "</div>\n";
-  }
-
-  $output .= "\n";
-
-  return $output;
 }
