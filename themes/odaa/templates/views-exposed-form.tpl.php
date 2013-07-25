ded0c212438a55b16dac3ae62e2f9a81d5a6ae98
Brian Jensen <mail@magnify.dk>
2013-07-25 13:39:25 +0200

    Removed unnecesary markup

--- themes/odaa/templates/views-exposed-form.tpl.php
+++ themes/odaa/templates/views-exposed-form.tpl.php
@@ -26,55 +26,47 @@
     print $q;
   ?>
 <?php endif; ?>
-<div class="views-exposed-form">
-  <div class="views-exposed-widgets clearfix search-form--content-block">
-    <?php foreach ($widgets as $id => $widget): ?>
-      <div id="<?php print $widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
-        <?php if (!empty($widget->label)): ?>
-          <label for="<?php print $widget->id; ?>">
-            <?php print $widget->label; ?>
-          </label>
-        <?php endif; ?>
-        <?php if (!empty($widget->operator)): ?>
-          <div class="views-operator">
-            <?php print $widget->operator; ?>
-          </div>
-        <?php endif; ?>
-        <div class="views-widget">
-          <?php print $widget->widget; ?>
+<div class="search-form--content-block">
+  <?php foreach ($widgets as $id => $widget): ?>
+    <div id="<?php print $widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
+      <?php if (!empty($widget->label)): ?>
+        <label for="<?php print $widget->id; ?>">
+          <?php print $widget->label; ?>
+        </label>
+      <?php endif; ?>
+      <?php if (!empty($widget->operator)): ?>
+        <div class="views-operator">
+          <?php print $widget->operator; ?>
         </div>
-        <?php if (!empty($widget->description)): ?>
-          <div class="description">
-            <?php print $widget->description; ?>
-          </div>
-        <?php endif; ?>
+      <?php endif; ?>
+      <div class="views-widget">
+        <?php print $widget->widget; ?>
       </div>
-    <?php endforeach; ?>
-    <div class="views-exposed-widget views-submit-button">
-      <?php print $button; ?>
+      <?php if (!empty($widget->description)): ?>
+        <div class="description">
+          <?php print $widget->description; ?>
+        </div>
+      <?php endif; ?>
     </div>
-    <?php if (!empty($reset_button)): ?>
-      <div class="views-exposed-widget views-reset-button">
-        <?php print $reset_button; ?>
-      </div>
-    <?php endif; ?>
+  <?php endforeach; ?>
+  <div class="views-exposed-widget views-submit-button">
+    <?php print $button; ?>
   </div>
-  <div class="forum--list-actions">
-    <?php if (!empty($sort_by)): ?>
-        <?php print $sort_by; ?>
-      <div class="views-exposed-widget views-widget-sort-order">
-        <?php print $sort_order; ?>
-      </div>
-    <?php endif; ?>
-    <?php if (!empty($items_per_page)): ?>
-      <div class="views-exposed-widget views-widget-per-page">
-        <?php print $items_per_page; ?>
-      </div>
-    <?php endif; ?>
-    <?php if (!empty($offset)): ?>
-      <div class="views-exposed-widget views-widget-offset">
-        <?php print $offset; ?>
-      </div>
-    <?php endif; ?>
-  </div>
+  <?php if (!empty($reset_button)): ?>
+    <div class="views-exposed-widget views-reset-button">
+      <?php print $reset_button; ?>
+    </div>
+  <?php endif; ?>
+</div>
+<div class="search--list-actions">
+  <?php if (!empty($sort_by)): ?>
+    <?php print $sort_by; ?>
+    <?php print $sort_order; ?>
+  <?php endif; ?>
+  <?php if (!empty($items_per_page)): ?>
+    <?php print $items_per_page; ?>
+  <?php endif; ?>
+  <?php if (!empty($offset)): ?>
+    <?php print $offset; ?>
+  <?php endif; ?>
 </div>
