40522eb59d1455a9930db79f53450cd9b2585cb4
Martin Yde <martinydegranath@Martins-MacBook-Pro.local>
2013-08-01 11:16:19 +0200

    added old comment id structure

--- themes/odaa/scripts/comment.js
+++ themes/odaa/scripts/comment.js
@@ -10,7 +10,7 @@
       var directive = {
         'li.comments--list-item': {
           'i<-data': {
-            '#comments--list-id' : 'i.cid',
+            '.comments--list-id' : 'i.cid',
             '.comments--list-author' : 'i.author',
             '.comments--list-posted' : 'i.date',
             '.comments--list-content' : 'i.subject'
