0fc83bd62c64322d39f515392d7b7783c159ca51
Thomas Johansen <thsj@aarhus.dk>
2013-08-22 12:55:19 +0200

    Reorder of scripts

--- themes/odaa/scripts/comment.js
+++ themes/odaa/scripts/comment.js
@@ -3,6 +3,8 @@
   var hash;
   $(document).ready(function($) {
     
+    setCommentActionLinks();
+    
     if ($('.comments--wrapper').length) {
       // Find hash.
       hash = $('.comments--wrapper').attr('data');
@@ -103,5 +105,50 @@
         $('.comments--post-comment-wrapper').hide();
       }
     });
+  }
+  
+  function setCommentActionLinks() {
+    /*
+     * Comment actions links.
+     */
+
+    // Set variables.
+    var commentsContent = $('.js-comments-content');
+
+    if (commentsContent.length) {
+      // Set variables.
+      var commentInput = $('.comments--post-comment textarea');
+      var commentInputValue = commentInput.val();
+
+      // Add quote link to actions wrapper.
+      $('<a />', {
+        'class' : 'comments--list-quote',
+        'href' : Drupal.t('#postcomment'),
+        'text' : Drupal.t('Quote')
+      })
+      .prependTo($('.comments--list-item-actions'))
+      .prepend('<i class="icon-quote-left"></i>')
+      .live('click', function() {
+        // Get content of comment.
+        var commentQuoteContent = $(this)
+        .parents('.js-comments-content')
+        .find('.field-comment-body')
+        .html();
+
+        commentInput.val(commentInputValue + '<blockquote>' + commentQuoteContent + '</blockquote>\n');
+      });
+
+      // Add icons to links.
+      var commentEditLink = $('.comments--list .comment-edit a');
+      var commentDeleteLink = $('.comments--list .comment-delete a');
+
+      if (commentEditLink.length) {
+        commentEditLink.prepend('<i class="icon-edit"></i>');
+      }
+
+      if (commentDeleteLink.length) {
+        commentDeleteLink.prepend('<i class="icon-remove-sign "></i>');
+      }
+    }
   }
 })(jQuery);
