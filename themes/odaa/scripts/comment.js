5ef6815ca9f559c21f6eb06f70f5d3c9d66dd33a
Thomas Johansen <thsj@aarhus.dk>
2013-08-22 10:02:38 +0200

    Test

--- themes/odaa/scripts/comment.js
+++ themes/odaa/scripts/comment.js
@@ -16,7 +16,7 @@
             '.comment-delete a@href+' : 'i.cid',
             '.comment-edit a@href+' : 'i.cid',
             '.comments--list-item-actions@class+' : function(arg) {
-              console.log('hi: ' + arg);
+              console.log(arg.item);
               if (arg.item.owner == false) {
                 return 'hide';
               }
